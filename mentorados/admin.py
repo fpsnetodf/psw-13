030c0b704267c946979a21726e7a2706d0a4b91a
fpsnetodf <fps_netodf@hotmail.com>
2025-04-02 12:58:15 -0300

    2 parte

--- mentorados/admin.py
+++ mentorados/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Mentorados, Navigators, DisponibilidadeHorarios
 # Register your models here.
+
+
+admin.site.register(Mentorados)
+admin.site.register(Navigators)
+admin.site.register(DisponibilidadeHorarios)
--- mentorados/views.py
+++ mentorados/views.py
@@ -1,12 +1,23 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render, redirect
-from .models import Navigators, Mentorados
+from .models import Navigators, Mentorados, DisponibilidadeHorarios
 from django.contrib.messages import add_message, constants
 
 # Create your views here.
+
 def mentorados(request):
     if request.method == 'GET':
         navigators = Navigators.objects.filter(user=request.user)
-        return render(request, 'mentorados/mentorados.html', {'estagios': Mentorados.estagio_choices, 'navigators': navigators})
+        mentorados = Mentorados.objects.filter(user=request.user)
+        
+        estagios_flat = [i[1] for i in Mentorados.estagio_choices]
+        qtd_estagios = []
+
+        for i, j in Mentorados.estagio_choices:
+            qtd_estagios.append(Mentorados.objects.filter(estagio=i).count())
+
+    
+        return render(request, 'mentorados/mentorados.html', {'estagios': Mentorados.estagio_choices, 'navigators': navigators, 'mentorados': mentorados, 'estagios_flat': estagios_flat, 'qtd_estagios': qtd_estagios})
     else:
         nome = request.POST.get('nome')
         foto = request.FILES.get('foto')
@@ -25,3 +36,30 @@ def mentorados(request):
 
         add_message(request, constants.SUCCESS, 'Mentorado cadastrado com sucesso.')
         return redirect('mentorados')
+
+def reunioes(request):
+    if request.method == 'GET':
+        return render(request, 'mentorados/reunioes.html')
+    else:
+        data = request.POST.get('data')
+
+        data = datetime.strptime(data, '%Y-%m-%dT%H:%M')
+
+        disponibilidade = DisponibilidadeHorarios.objects.filter(
+            data_inicial__gte=(data - timedelta(minutes=50)),
+            data_inicial__lte=(data + timedelta(minutes=50))
+        )
+
+        if disponibilidade.exists():
+            add_message(request, constants.ERROR, 'Você já possui uma reunião em aberto.')
+            return redirect('reunioes')
+
+        disponibilidade = DisponibilidadeHorarios(
+            data_inicial=data,
+            mentor=request.user
+
+        )
+        disponibilidade.save()
+
+        add_message(request, constants.SUCCESS, 'Horário disponibilizado com sucesso.')
+        return redirect('reunioes')
